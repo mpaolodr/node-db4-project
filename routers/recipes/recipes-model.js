8f8d9eceb66654aaff69d97ae00d5edb76960d31
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-20 13:51:57 -0500

    added getShoppingList function and GET method to retrieve list of ingredients and quantity

--- routers/recipes/recipes-model.js
+++ routers/recipes/recipes-model.js
@@ -4,6 +4,21 @@ function getRecipes() {
   return db("recipes");
 }
 
+function getShoppingList(id) {
+  return db("recipes")
+    .where({ id })
+    .first()
+    .then(recipe => {
+      return db("recipe_ingredients as ri")
+        .join("recipes as r", "ri.recipe_id", "r.id")
+        .join("ingredients as i", "ri.ingredient_id", "i.id")
+        .select("i.id", "i.name as Ingredient", "ri.quantity")
+        .where({ "ri.recipe_id": id })
+        .orderBy("ri.ingredient_id");
+    });
+}
+
 module.exports = {
-  getRecipes
+  getRecipes,
+  getShoppingList
 };
--- routers/recipes/recipesRouter.js
+++ routers/recipes/recipesRouter.js
@@ -11,4 +11,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id/shoppinglist", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const list = await Rec.getShoppingList(id);
+
+    if (list.length !== 0) {
+      res.status(200).json(list);
+    } else {
+      res.status(404).json({ errorMessage: "Invalid ID" });
+    }
+  } catch (err) {
+    res.status(500).json({ errorMessage: err.message });
+  }
+});
+
 module.exports = router;
